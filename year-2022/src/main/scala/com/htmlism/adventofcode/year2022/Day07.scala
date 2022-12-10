fe90f2ecdab1d42ec41129c0551f06b72a3037d2
Mark Canlas <github@htmlism.com>
2022-12-10 08:25:09 -0500

    day 7 part one

--- year-2022/src/main/scala/com/htmlism/adventofcode/year2022/Day07.scala
+++ year-2022/src/main/scala/com/htmlism/adventofcode/year2022/Day07.scala
@@ -10,7 +10,28 @@ object Day07:
   def apply(part: Part)(xs: List[String]): String =
     xs
       .map(TerminalOutput.parse)
-      .mkString("\n")
+      .traverse(toStatelessCmd)
+      .runA(CurrentWorkingDirectory.empty)
+      .value
+
+      // drop terminal output that did not produce absolute file paths
+      .flatMap(_.toList)
+      .pipe { xs =>
+        val uniqueDirs =
+          xs
+            .map(_.slug)
+            .distinct
+
+        uniqueDirs
+          .map { slug =>
+            xs
+              .filter(_.slug.startsWith(slug))
+              .map(_.x.size)
+              .sum
+          }
+          .filter(_ < 100000)
+          .sum
+      }
       .toString
 
   object Pat:
@@ -38,21 +59,61 @@ object Day07:
               GoUpDirectory
 
             case d =>
-              AddDirectory(d)
+              ChangeDirectory(d)
 
         case Pat.List() =>
           List
 
         case Pat.Dir(d) =>
-          AddDirectory(d)
+          ChildDirectory(d)
 
         case Pat.File(s, f) =>
-          AddFile(s.toInt, f)
+          ChildFile(s.toInt, f)
+
+  sealed trait Node(val size: Int)
 
   enum TerminalOutput:
     case ToRoot
     case List
-    case AddDirectory(s: String)
-    case AddFile(size: Int, name: String)
+    case ChildDirectory(s: String)              extends TerminalOutput with Node(0)
+    case ChildFile(fileSize: Int, name: String) extends TerminalOutput with Node(fileSize)
     case ChangeDirectory(s: String)
     case GoUpDirectory
+
+  // newtypes save me
+  case class CurrentWorkingDirectory(xs: List[String]):
+    def push(s: String) =
+      copy(xs = xs :+ s)
+
+    def pop =
+      copy(xs = xs.slice(0, xs.size - 1))
+
+  case class Path(xs: List[String], x: Node):
+    def slug =
+      "/" + xs.mkString("/")
+
+  object CurrentWorkingDirectory:
+    val empty =
+      CurrentWorkingDirectory(Nil)
+
+  def toStatelessCmd(cmd: TerminalOutput) =
+    State { (cwd: CurrentWorkingDirectory) =>
+      cmd match
+        case TerminalOutput.ToRoot =>
+          CurrentWorkingDirectory.empty -> None
+
+        case TerminalOutput.List =>
+          cwd -> None
+
+        case d: TerminalOutput.ChildDirectory =>
+          cwd -> Path(cwd.xs, d).some
+
+        case f: TerminalOutput.ChildFile =>
+          cwd -> Path(cwd.xs, f).some
+
+        case TerminalOutput.ChangeDirectory(d) =>
+          cwd.push(d) -> None
+
+        case TerminalOutput.GoUpDirectory =>
+          cwd.pop -> None
+    }
