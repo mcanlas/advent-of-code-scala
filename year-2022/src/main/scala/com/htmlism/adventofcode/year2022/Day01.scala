80f909985c2caa185dce793652e398827e492826
Mark Canlas <github@htmlism.com>
2022-12-13 15:04:10 -0500

    drop state

--- year-2022/src/main/scala/com/htmlism/adventofcode/year2022/Day01.scala
+++ year-2022/src/main/scala/com/htmlism/adventofcode/year2022/Day01.scala
@@ -7,19 +7,14 @@ import cats.syntax.all.*
 object Day01:
   def apply(take: Int)(xs: List[String]): String =
     xs
-      .traverse(accCmd)
-      .runS(NonEmptyList.one(Nil))
-      .value
+      .foldLeft(NonEmptyList.one(List.empty[Int]))((xs, cmd) =>
+        if (cmd.isEmpty)
+          (Nil :: xs)
+        else
+          NonEmptyList(cmd.toInt :: xs.head, xs.tail)
+      )
       .map(_.sumAll)
       .sorted(Order.reverse(Order[Int]))
       .take(take)
       .sumAll
       .toString
-
-  private def accCmd(cmd: String) =
-    State { (xs: NonEmptyList[List[Int]]) =>
-      if (cmd.isEmpty)
-        (Nil :: xs)                                 -> cmd
-      else
-        NonEmptyList(cmd.toInt :: xs.head, xs.tail) -> cmd
-    }
